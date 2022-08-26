5f8913aac2b4b1cdeb1631727772c5237a58e665
Aleksey <pilot-1981@yandex.ru>
2022-08-26 17:18:30 +0500

    add Sem_2=Task_1

---
+++ Sem_2/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите делимое = "); 
+int num1 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите делитель = "); 
+int num2 = int.Parse(Console.ReadLine());
+int rem = 0;
+
+if (num1 % num2 == 0)
+{
+    Console.WriteLine ("Число " + num1 + " кратно числу " + num2);
+}
+else 
+{
+    rem = num1 % num2;
+    Console.WriteLine ("Число " + num1 + " не кратно числу " + num2 + " при делении остаток равен " + rem);
+}
