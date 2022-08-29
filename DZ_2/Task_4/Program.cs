1a64e7c56051eaa934ea1b9d7e5f9559c23ca05d
Aleksey <pilot-1981@yandex.ru>
2022-08-29 15:25:52 +0500

    add DZ_Task_4.1

---
+++ DZ_2/Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1 = new Random().Next(1, 1000000); 
+Console.WriteLine(num1); 
+int num2 = 7;
+int num3 = 23;
+
+if (num1 % num2 == 0 && num1 % num3 == 0)
+{
+    Console.WriteLine ("Число " + num1 + " кратно числам " + num2 + " и " + num3);
+}
+else 
+{
+    Console.WriteLine ("Число " + num1 + " не кратно числам " + num2 + " и " + num3);
+}
