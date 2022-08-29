8ad1fc227b71a467df9da4cc631164724ef68e59
Aleksey <pilot-1981@yandex.ru>
2022-08-29 14:19:02 +0500

    add DZ_Task_2.1

---
+++ DZ_2/Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int rand = new Random().Next(1, 65536);
+Console.WriteLine(rand);
+if (rand > 99)
+{
+    Console.WriteLine(rand.ToString() [2]);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет!!!");
+}
