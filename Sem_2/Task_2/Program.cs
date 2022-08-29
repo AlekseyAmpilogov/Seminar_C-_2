4afa5a7787a32e273dff177c602afc7ed602ee17
Aleksey <pilot-1981@yandex.ru>
2022-08-29 09:42:12 +0500

    add Task_2.2

--- Sem_2/Task_2/Program.cs
+++ Sem_2/Task_2/Program.cs
@@ -2,4 +2,4 @@
 Console.WriteLine(rand);
 int first_digit = rand / 100;
 int last_digit = rand % 10;
-Console.WriteLine(first_digit + "" + last_digit);
+Console.WriteLine(first_digit.ToString() + last_digit.ToString());
