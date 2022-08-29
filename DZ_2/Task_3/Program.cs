559b209c2e233c48014ed74579701801bb89d62f
Aleksey <pilot-1981@yandex.ru>
2022-08-29 15:01:28 +0500

    add DZ_Task_3.2

---
+++ DZ_2/Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿Start:
+
+Console.WriteLine ("Введите цифру, обозначающую день недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 || day ==7)
+{
+    Console.WriteLine("Это выходной день!");
+}
+else 
+    if (day < 1 || day > 7)
+    {
+        Console.WriteLine("Это не день недели!");
+    }
+else 
+    if (day > 0 && day < 6)
+    {
+        Console.WriteLine("Это рабочий день!");
+    }
+
+goto Start;
